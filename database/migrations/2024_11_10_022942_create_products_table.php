da87c100da31b8a9473c8ea35f9496929571fc8e
Ade Randika <aderandika@MacBook-Pro-Ade.local>
2024-11-11 17:21:23 +0700

    update pendaftaran user

---
+++ database/migrations/2024_11_10_022942_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('cover');
+            $table->unsignedBigInteger('price');
+            $table->text('about');
+            $table->string('path_file');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            // Jika nama kolom yang berelasi berbeda
+            $table->unsignedBigInteger('creator_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            //Relasi jika lebih detail 
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
